3d6f2367ef3ff0e6adcdd1dccae4c8a13272510f
vsustrov <vsustrov@uwaterloo.ca>
2023-05-22 16:45:28 -0400

    added new problems

--- stacks/validParentheses.py
+++ stacks/validParentheses.py
@@ -1,25 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
+        # map each closing bracket to corresponding opening bracket
+        brackets_map = {"]":"[", ")":"(", "}":"{"}
 
-        #iterate through string
-        for i in range(0, len(s)):
-            # get top of stack (or 0 if no stack exists)
-            if len(stack) > 0:
-                stack_top = stack[len(stack) - 1]
-            else:
-                stack_top = '0'
-            # push onto stack
-            if s[i] == '[' or s[i] == '(' or s[i] == '{':
-                stack.append(s[i])
-            # pop from stack
-            elif (s[i] == ']' and stack_top == '[') or (s[i] == ')' and stack_top == '(') or (s[i] == '}' and stack_top == '{'):
-                # pop from stack
-                stack.pop(len(stack) - 1)
-            else:
-                # invalid bracket
-                return False
+        # every time we encounter a closing bracket, check the top of the stack
+        b_stack = []
 
-        return len(stack) == 0
+        for char in s:
+            # 2 cases: opening bracket or closing bracket
+            if char in brackets_map: # closing bracket
+                if len(b_stack) and b_stack[-1] == brackets_map[char]:
+                    b_stack.pop()
+                else:
+                    return False
+            
+            else:
+                b_stack.append(char)
+        
+        # important: check that stack is empty after we've gone through entire string
+        if len(b_stack) == 0:
+            return True
+        return False
+
 
     # this solution has complexity O(n)
